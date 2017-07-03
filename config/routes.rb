6b15c1767e630a1d9e154448ade1cfeaee5106ba
Dominic Brown <yeahdom@gmail.com>
2017-07-03 10:40:57 -0700

    add skeleton

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+
+  # resources :users
+  # resources :sessions
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+end
